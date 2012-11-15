0883a6a45848e8111d751ed9aa9397396cbc3907
mrios <riosmatias.zarate@gmail.com>
2012-11-15 18:57:20 -0300

    Add HAL Collection Pageable ('next', 'prev' links) & HAL Collection Scrollable ('more' link)

--- sigma/hal/collections.js
+++ sigma/hal/collections.js
@@ -1,5 +1,5 @@
 steal(
-	'sigma/lib/parseuri.js'
+    'sigma/lib/parseuri.js'
 ,	'sigma/lib/uritemplates.js'
 ,	'sigma/lib/hal'
 ,	'sigma/hal/hal_builder.js'
@@ -7,26 +7,53 @@ steal(
 ).then(
 	function()
 	{
-		Sigma.fixtures=Sigma.fixtures||{}
-		Sigma.fixtures.collectionsFixturator
-		=	function(collection_dfd)
-			{
-			return collection_dfd
-				.then(
-					function(collection)
+		can.Construct(
+			'Sigma.fixtures.collection'
+		,	{
+				defaults:
 					{
-					var generator
-					=	function(original)
-						{
-						var	parsed
-							=	parseUri(original.url)
-						,	current_page
-							=	parseInt(parsed.queryKey.page)||undefined
-						,	items_per_page
-							=	parseInt(parsed.queryKey['items-per-page'])||10
-						,	name
-							=	parsed.path.split('/').pop()
-								return	 Sigma.fixtures
+						assets_path:'hal/fixtures'
+					,	ext:'.csv'
+					}
+			,	getCollection: 
+					function(uri)
+					{
+					var	data
+					=	parseUri(uri)
+					,	name
+					=	data.directory.match(/\/?([^\/]*)/)[1]
+					return	Sigma.fixtures
+						.store.get(this.defaults.assets_path+'/'+name+this.defaults.ext)
+						.pipe(
+							function(results)
+							{
+							return	{
+									name:name
+								,	items: results
+								}
+							}
+						)
+				}
+			,	getCollectionsFixturator: 
+					function(collection_dfd)
+					{
+					var	self=this
+					return	collection_dfd
+						.then(
+							function(collection)
+							{
+							var	generator 
+							=	function(original)
+								{
+								var	parsed
+								=	parseUri(original.url)
+								,	current_page
+								=	parseInt(parsed.queryKey.page)||undefined
+								,	items_per_page
+								=	parseInt(parsed.queryKey['items-per-page'])||10
+								,	name
+								=	parsed.path.split('/').pop()
+								return	 self
 									.getPage(
 										collection
 									,	{
@@ -35,62 +62,33 @@ steal(
 										,	collectionUrl:'/'+name
 										}
 									)
-						}
-						can.fixture("GET /"+collection.name ,	generator)
-						can.fixture("GET /"+collection.name+"?page={p}" ,	generator)
-						can.fixture("GET /"+collection.name+"?page={p}&items-per-page={ipp}" ,	generator)
-						can.fixture("GET /"+collection.name+"?items-per-page={ipp}" ,	generator)
-					}
-				)
-			}
-		Sigma.fixtures.getCollection
-		=	function(uri)
-			{
-				var assets_path = "hal/fixtures/", ext = ".csv"
-				var	data
-						=parseUri(uri)
-				,	name
-						=data.directory.match(/\/?([^\/]*)/)[1]
-				return Sigma.fixtures.store.get(assets_path+name+ext)
-					.pipe(
-						function(results)
-						{
-						return	{
-								name:name
-							,	items: results
+								}
+								can.fixture("GET /"+collection.name ,	generator)
+								can.fixture("GET /"+collection.name+"?page={p}" ,	generator)
+								can.fixture("GET /"+collection.name+"?page={p}&items-per-page={ipp}" ,	generator)
+								can.fixture("GET /"+collection.name+"?items-per-page={ipp}" ,	generator)
 							}
-						}
-					)
-			}
-		Sigma.fixtures.getPage
-		=	function(collection,options)
-			{
-				var	current_page
-						=options.currentPage||1
-				,	items_per_page
-						=options.itemsPerPage||20
-				,	start
-						=(current_page-1)*items_per_page
-				,	next_page
-						=1+current_page
-				,	prev_page
-						=current_page-1
-				,	end
-						=start+items_per_page
-				,	url
-						=uritemplate(options.collectionUrl+'{/id}{?page,items-per-page}')
-				,	last_page
-						=(collection.items.length+items_per_page-1)/items_per_page
-				,	self
-						={self:{href:url.expand({page:(current_page!=1)?current_page:null})}}
-				,	next
-						={next:{href:url.expand({page:next_page,'items-per-page':items_per_page})}}
-				,	prev
-						={prev:{href:url.expand({page:prev_page,'items-per-page':items_per_page})}}
-				,	links
-						=self
-				,	gen_items
-						=function(start,end)
+						)
+					}
+			,	getPage: 
+					function(collection,options)
+					{
+					var	current_page
+					=	options.currentPage||1
+					,	items_per_page
+					=	options.itemsPerPage||20
+					,	start
+					=	(current_page-1)*items_per_page
+					,	end
+					=	start+items_per_page
+					,	url
+					=	uritemplate(options.collectionUrl+'{/id}{?page,items-per-page}')
+					,	last_page
+					=	(collection.items.length+items_per_page-1)/items_per_page
+					,	self
+					=	{self:{href:url.expand({page:(current_page!=1)?current_page:null})}}
+					,	gen_items
+					=	function(start,end)
 						{
 						return	can.map(
 								collection.items
@@ -104,17 +102,64 @@ steal(
 								}
 							).slice(start,end)
 						}
-				return	new Sigma.fixtures
-					.hal_builder({},self.href)
-					.link({self:{href:url.expand({})}})
-					.link(prev,prev_page>0)
-					.link(next,next_page<=last_page)
-					.embedded(
-						{'collection':
-							gen_items(start,end)
-						}
-					)
-					.get_document()
+					,	hal
+					=	new Sigma.fixtures
+							.hal_builder({},self.href)
+							.link({self:{href:url.expand({})}})
+							.embedded(
+								{
+									'collection':gen_items(start,end)
+								}
+							)
+					return	this
+						.addLinks(
+							hal
+						, 	{
+								url: url
+							,	current_page: current_page
+							,	items_per_page: items_per_page
+							,	last_page: last_page
+							}
+						).get_document()
+					}
 			}
+		,	{}
+		)
+		Sigma.fixtures.collection(
+			'Sigma.fixtures.collection.pageable'
+		,	{
+				addLinks: 
+					function(hal,config)
+					{
+					var	next_page
+					=	1+config.current_page
+					,	prev_page
+					=	config.current_page-1
+					,	next
+					=	{next:{href:config.url.expand({page:next_page,'items-per-page':config.items_per_page})}}
+					,	prev
+					=	{prev:{href:config.url.expand({page:prev_page,'items-per-page':config.items_per_page})}}
+					return	hal
+						.link(prev,prev_page>0)
+						.link(next,next_page<=config.last_page)
+					}
+			}
+		,	{}
+		)
+		Sigma.fixtures.collection(
+			'Sigma.fixtures.collection.scrollable'
+		,	{
+				addLinks: 
+					function(hal,config)
+					{
+					var	more_page
+					=	1+config.current_page		
+					,	more
+					=	{more:{href:config.url.expand({page:more_page,'items-per-page':config.items_per_page})}}
+					return	hal.link(more,more_page>0 && more_page<=config.last_page)
+					}
+			}
+			,{}
+		)
 	}
 )
