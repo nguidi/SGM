4670f6ac6d08f98a8a8aa028db4e4f90f50fdf48
gpittau <gpittau@gmail.com>
2012-11-22 05:08:09 -0300

    Cosmetic (white space only) 	modified:   sigma/stream/stream_fixture.js 	modified:   sigma/stream/stream_test.js

--- sigma/stream/stream_fixture.js
+++ sigma/stream/stream_fixture.js
@@ -1,75 +1,76 @@
 steal(
- 'sigma/hal/hal_builder.js'
+	'sigma/hal/hal_builder.js'
 ,	function()
 	{
-             can.fixture(
-                   'GET /comments'
-                ,	function()
-                   {
-                              var genActions = function(url)
+		can.fixture(
+			'GET /comments'
+		,	function()
+			{
+			var	genActions
+			=	function(url)
 				{
-                                   return can.map(
-                                                [
-                                                      {id: 'me-gusta', value:'Me Gusta', icon: 'thumbs-up'}
-                                          ,	{id:'plusone', value:'1', icon: 'plus'}
-                                              ]
-                                      ,	function(action)
-                                             {
-                                                      return 	{
-                                                                      title: action.value
-                                                            ,	icon: action.icon
-                                                            ,	align: 'right'
-                                                               ,	href: url+'/actions/'+action.id
-                                                              }
+				return can
+					.map(
+						[
+							{id: 'me-gusta', value:'Me Gusta', icon: 'thumbs-up'}
+						,	{id:'plusone', value:'1', icon: 'plus'}
+						]
+					,	function(action)
+						{
+						return	{
+								title: action.value
+							,	icon: action.icon
+							,	align: 'right'
+							,	href: url+'/actions/'+action.id
+							}
 						}
-                                      )
+					)
 				}
-
-                               var genComments = function()
+			var	genComments
+			=	function()
 				{
-                                     return	can.map(
-                                                [
-                                                      {owner: 'TED', value: 'Hola Mi Nombre es Ted'}
-                                         ,	{owner: 'NERD', value: 'Hola Mi Nombre es Nerd'}
-                                             ,	{owner: 'Friky', value: 'Hola Mi Nombre es Friky'}
-                                           ]
-                                      ,	function(comment)
-                                            {
-                                                      return  new Sigma.fixtures
-                                                                     .hal_builder(
-                                                                          {
-                                                                                      title: comment.owner
-                                                                           ,	icon:'sitemap'
-                                                                               ,	description: comment.owner+':'+comment.value
-                                                                         }
-                                                                      ,	'/comments'+'/'+comment.owner
-                                                                        ).link(
-                                                                                {
-                                                                                      'actions': genActions('/comments'+'/'+comment.owner)
-                                                                           }
-                                                                      )
-                                              }
-                                      )
+				return	can
+					.map(
+						[
+							{owner: 'TED', value: 'Hola Mi Nombre es Ted'}
+						,	{owner: 'NERD', value: 'Hola Mi Nombre es Nerd'}
+						,	{owner: 'Friky', value: 'Hola Mi Nombre es Friky'}
+						]
+					,	function(comment)
+						{
+						return  new Sigma.fixtures
+							.hal_builder(
+								{
+									title: comment.owner
+								,	icon:'sitemap'
+								,	description: comment.owner+':'+comment.value
+								}
+							,	'/comments'+'/'+comment.owner
+							).link(
+								{
+									'actions': genActions('/comments'+'/'+comment.owner)
+								}
+							)
+						}
+					)
 				}
-
-                               return  new Sigma.fixtures
-                                             .hal_builder(
-                                                  {
-                                                      }
-                                              ,	'/stream'
-                                            ).link(
-                                                        {
-                                                              'comments':
-                                                            {
-                                                                      href:'/comments'
-                                                               }
-                                                      }
-                                              ).embedded(
-                                                    {
-                                                              'comments': genComments()
-                                                      }
-                                              ).get_document()
-                       }
-              )
+			return	new Sigma.fixtures
+				.hal_builder(
+					{ }
+				,	'/stream'
+				).link(
+					{
+					'comments':
+						{
+						href:'/comments'
+						}
+					}
+				).embedded(
+					{
+					'comments': genComments()
+					}
+				).get_document()
+			}
+		)
 	}
 )
