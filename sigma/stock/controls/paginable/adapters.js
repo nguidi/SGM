942892edf8fe4c8c32cec744fbbea8c8b2dd2db7
nguidi <neri3004@gmail.com>
2012-11-30 20:27:14 -0300

    Paginable - Scrollable - Fix Provincias

---
+++ sigma/stock/controls/paginable/adapters.js
@@ -0,0 +1,27 @@
+steal(
+	'sigma/stock/controls/lib'
+).then(
+	function()
+	{
+		Sigma.Model.HAL.Resource(
+			'Sigma.Model.HAL.Collection'
+		,	{}
+		,	{
+				getCollection:function()
+				{
+					return	this.embedded.attr('collection')
+				}
+			,	getPrev: function()
+				{
+					return this.links.attr('prev')
+				}
+			,	getNext: function()
+				{
+					return this.links.attr('next')
+				}
+			}
+		)
+
+		can.Model.List( 'Sigma.Model.HAL.Collection.List');
+	}
+)
---
+++ sigma/stock/controls/scrollable/scrollable.js
@@ -0,0 +1,113 @@
+steal(
+	'sigma/lib'
+,	'sigma/lib/hypermedia.js'
+,	'sigma/hal/collections.js'
+).then(
+	function() {
+		Sigma.HypermediaControl(
+			'Sigma.Controls.Scrollable'
+		,	{
+				defaults:{
+					view_more: '//sigma/stock/controls/scrollable/views/more.mustache'
+				,	view_content: false
+				}
+			}
+		,	{
+				init: function(element,options)
+				{
+					can.append(
+						this.element
+					,	can.$('<div class="	content">')
+					)
+
+					can.append(
+						this.element
+					,	can.$('<div class="more">')
+					)					
+
+					can.append(
+						this.element
+							.find('div.content')
+					,	can.view(
+							this.options.view_content
+						,	this.options.slot
+						)
+					)
+
+					can.append(
+						this.element
+							.find('div.more')
+					,	can.view(
+							this.options.view_more
+						,	this.options.slot
+						)
+					)
+				}
+
+			,	update: function()
+				{
+					this.updateContent()
+					this.updateMore()
+				}
+
+			, 	updateContent: function ()
+				{
+					can.append(
+						this.element
+							.find('div.content')
+					,	can.view(
+							this.options.view_content
+						,	this.options.slot
+						)
+					)
+				}
+
+			,	updateMore: function() 
+				{
+					this.element
+						.find('div.more')
+						.empty()
+
+					can.append(
+						this.element
+							.find('div.more')
+					,	can.view(
+							this.options.view_more
+						,	this.options.slot
+						)
+					)
+			}
+
+			,	'button.more click' : function(element,event)
+				{
+					var self = this
+					Sigma.Model.HAL.Collection.getRoot(element.attr('scroll-href'),'scrollable')
+					.then(
+						function(slot)
+						{
+							self.options.slot = slot 
+							self.update()
+						}
+					)
+				}
+
+			,	'{window} scroll': function(el,ev)
+				{
+					if (can.$(el).height() + can.$(el).scrollTop() == can.$(document).height())
+					{
+						var self = this
+						Sigma.Model.HAL.Collection.getRoot(self.options.slot.attr('more').attr('href'),'scrollable')
+						.then(
+							function(slot)
+							{
+								console.log(slot)
+								self.options.slot = slot 
+								self.update()
+							}
+						)
+					}
+				}
+			}
+		)
+	}
+)
