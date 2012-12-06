f95135db230f42618d58c54046ccf13fb625f09c
nguidi <neri3004@gmail.com>
2012-12-06 16:41:16 -0300

    Scrollable - New Test

---
+++ sigma/stock/controls/scrollable/tests.js
@@ -0,0 +1,97 @@
+steal(
+	'sigma/stock/controls/scrollable'
+,	'sigma/stock/controls/scrollable/adapters.js'
+).then(
+	function()
+	{
+		module(
+			"sigma/stock/controls"
+		)
+		test(
+			"Scrollable"
+		,	function()
+			{
+				var 	scrollable_container
+				,	scrollableHTML = can.$('<div id="scrollableContainer">')
+
+				Sigma.HypermediaContainer(
+					'Sigma.Hypermedia.Scrollable.Container'
+				,	{
+						defaults:
+						{
+							media_types:
+							{
+								'scrollable':
+								{
+									Handler: Sigma.Controls.Scrollable
+								,	options:
+									{
+										target: 'scrollable'
+									,	view_content: '//sigma/stock/controls/scrollable/views/content.mustache'
+									}
+								}
+							}
+						}
+					}
+				,	{
+					}
+				)
+
+				Sigma.fixtures.collection(
+					'Scrollable'
+				,	{}
+				,	{}
+				)
+				
+				Scrollable.defaults 
+				= 	{
+						assets_path:'../fixtures/data/json'
+					,	ext:'.json'
+					}
+
+				stop()
+				Sigma.fixtures.collection.scrollable.getCollectionsFixturator(
+						Scrollable.getCollection("/pageable-scrollable")
+				).then(
+					function()
+					{
+						start()
+						scrollable_container = new Sigma.Hypermedia.Scrollable.Container(
+							scrollableHTML
+						,	{
+								id:'Scrollable'
+							,	target: 'Scrollable'
+							,	slot: 	Sigma.Model.HAL.Collection.getRoot('/pageable-scrollable?page=1&items-per-page=5','scrollable')
+							}
+						)
+
+						stop()
+						scrollable_container.options.slot
+							.then(
+								function(page1)
+								{
+									start()
+									ok(page1.links, "links OK");
+									ok(page1.embedded, "embedded OK");
+									ok(page1.links.more, "links.more OK");
+									ok(page1.embedded.collection, "_embedded.items OK");
+									stop()
+									page1.links.more.fetch()
+										.then(
+											function(page2)
+											{
+												start()
+												ok(page2.links, "links OK");
+												ok(page2.embedded, "embedded OK");
+												ok(page2.links.more, "links.more OK");
+												ok(page2.embedded.collection, "_embedded.items OK");
+											}
+										)
+								}
+							)
+					}
+				)
+			}
+		)
+	}
+)
