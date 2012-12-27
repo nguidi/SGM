f6658c9894ccc0c1ff1018885ea17cb84a759d0b
nguidi <neri3004@gmail.com>
2012-12-27 15:27:38 -0300

    Pageable Browse Ready

--- sigma/stock/controls/pageable/pageable.js
+++ sigma/stock/controls/pageable/pageable.js
@@ -1,122 +1,130 @@
-Sigma.HypermediaControl(
-	'Sigma.Controls.Pageable'
-,	{
-		defaults:{
-			view_links: false
-		,	view_content: false
-		}
-	}
-,	{
-		_render_content: function(data)
-		{
-			var exists = 	this.element.find('div').hasClass('content') 
-			||		this.element.find('div').hasClass('links')
+steal(
+	'sigma/stock/controls/lib'
+,	'sigma/hal/collections.js'
+).then(
+	function()
+	{
+		Sigma.HypermediaControl(
+			'Sigma.Controls.Pageable'
+		,	{
+				defaults:{
+					view_links: false
+				,	view_content: false
+				}
+			}
+		,	{
+				_render_content: function(data)
+				{
+					var exists = 	this.element.find('div').hasClass('content') 
+					||		this.element.find('div').hasClass('links')
 
-			if (!exists) {
-				this._render_pageable_content(data)
-				this._render_pageable_links(data)
-			} 
-			this._update_content(data)			
-		}
+					if (!exists) {
+						this._render_pageable_content(data)
+						this._render_pageable_links(data)
+					} 
+					this._update_content(data)			
+				}
 
-	,	_render_pageable_content: function(data)
-		{
-			can.append(
-				this.element
-			,	can.$('<div class="content">')
-			)
+			,	_render_pageable_content: function(data)
+				{
+					can.append(
+						this.element
+					,	can.$('<div class="content">')
+					)
 
-			can.append(
-				this.element
-					.find('div.content')
-			,	can.view(
-					this.options.view_content
-				,	data
-				)
-			)
-		}
+					can.append(
+						this.element
+							.find('div.content')
+					,	can.view(
+							this.options.view_content
+						,	data
+						)
+					)
+				}
 
-	,	_render_pageable_links: function(data)
-		{
-			can.append(
-				this.element
-			,	can.$('<div class="links">')
-			)
+			,	_render_pageable_links: function(data)
+				{
+					can.append(
+						this.element
+					,	can.$('<div class="links">')
+					)
 
-			can.append(
-				this.element
-					.find('div.links')
-			,	can.view(
-					this.options.view_links
-				,	data
-				)
-			)
-		}
+					can.append(
+						this.element
+							.find('div.links')
+					,	can.view(
+							this.options.view_links
+						,	data
+						)
+					)
+				}
 
-	,	_update_content: function(data)
-		{
-			// update pageable content
-			this.element
-				.find('div.content')
-				.empty()
+			,	_update_content: function(data)
+				{
+					// update pageable content
+					this.element
+						.find('div.content')
+						.empty()
 
-			can.append(
-				this.element
-					.find('div.content')
-			,	can.view(
-					this.options.view_content
-				,	data
-				)
-			)
+					can.append(
+						this.element
+							.find('div.content')
+					,	can.view(
+							this.options.view_content
+						,	data
+						)
+					)
 
-			// update pageable links
-			this.element
-				.find('div.links')
-				.empty()
+					// update pageable links
+					this.element
+						.find('div.links')
+						.empty()
 
-			can.append(
-				this.element
-					.find('div.links')
-			,	can.view(
-					this.options.view_links
-				,	data
-				)
-			)
+					can.append(
+						this.element
+							.find('div.links')
+					,	can.view(
+							this.options.view_links
+						,	data
+						)
+					)
 
-			if (data.attr('prev'))
-				this.element
-					.find('ul.paginable')
-					.find('li.previous')
-					.removeClass('disabled')
-			else
-				this.element
-					.find('ul.paginable')
-					.find('li.previous')
-					.addClass('disabled')
+					if (data.attr('prev'))
+						this.element
+							.find('ul.paginable')
+							.find('li.previous')
+							.removeClass('disabled')
+					else
+						this.element
+							.find('ul.paginable')
+							.find('li.previous')
+							.addClass('disabled')
 
-		        if (data.attr('next'))
-				this.element
-					.find('ul.paginable')
-					.find('li.next')
-					.removeClass('disabled')
-			else
-				this.element
-					.find('ul.paginable')
-					.find('li.next')
-					.addClass('disabled')
-		}
+				        if (data.attr('next'))
+						this.element
+							.find('ul.paginable')
+							.find('li.next')
+							.removeClass('disabled')
+					else
+						this.element
+							.find('ul.paginable')
+							.find('li.next')
+							.addClass('disabled')
+				}
 
-	,	'ul.paginable li:not(".disabled") click' : function(element,event)
-		{
-			// Tengo q tratarlo asi porque pierde el Resource Type 
-			// y el Rel entonces no me lo reconoce el HConteiner
-			this._update(
-				this.options.slot.constructor.getRoot(
-					element.data('link').href
-				,	this.options.slot.rel
-				)
-			)	
-			// this._update(element.data('link').fetch())
-		}
+			,	'ul.paginable li:not(".disabled") click' : function(element,event)
+				{
+					// Tengo q tratarlo asi porque pierde el Resource Type 
+					// y el Rel entonces no me lo reconoce el HConteiner
+					this._update(
+						this.options.slot.constructor.getRoot(
+							element.data('link').href
+						,	this.options.slot.rel
+						)
+					)	
+					// this._update(element.data('link').fetch())
+				}
+			}
+		)
 	}
 )
--- sigma/stock/controls/pageable/tests.js
+++ sigma/stock/controls/pageable/tests.js
@@ -36,18 +36,19 @@ steal(
 					}
 				,	{
 					}
+				
 				)
 
-				Sigma.fixtures.collection(
-					'Pageable'
-				,	{}
-				,	{}
+				can.fixture(
+					'GET /pageable-scrollable'
+				,	steal
+						.idToUri("//stock/fixtures/data/json/pageable-scrollable.json")
+						.path
 				)
 
-				can.fixture('GET /pageable-scrollable',steal.idToUri("//stock/fixtures/data/json/pageable-scrollable.json").path)
 				stop()
 				Sigma.fixtures.collection.pageable.getCollectionsFixturator(
-						Pageable.getCollection("/pageable-scrollable")
+					Sigma.fixtures.collection.getCollection("/pageable-scrollable")
 				).then(
 					function()
 					{
@@ -57,10 +58,9 @@ steal(
 						,	{
 								id: 'Pageable'
 							,	target: 'Pageable'
-							,	slot: Sigma.Model.HAL.Collection.getRoot('/pageable-scrollable?page=1&items-per-page=5','pageable')
+							,	slot: Sigma.Model.HAL.Collection.getRoot('/pageable-scrollable?items-per-page=5','pageable')
 							}
 						)
-
 						equal(pageable_container.options.id,"Pageable","ID Generated")
 
 						stop()
@@ -69,21 +69,21 @@ steal(
 								function(page1)
 								{
 									start()
-									ok(page1.links, "links OK");
-									ok(page1.embedded, "embedded OK");
-									ok(page1.links.next, "links.next OK");
-									ok(page1.embedded.collection, "_embedded.items OK");
+									ok(page1.links, "Page 1: links OK");
+									ok(page1.embedded, "Page 1: embedded OK");
+									ok(page1.links.next, "Page 1: links.next OK");
+									ok(page1.embedded.collection, "Page 1: _embedded.items OK");
 									stop()
 									page1.links.next.fetch()
 										.then(
 											function(page2)
 											{
 												start()
-												ok(page2.links, "links OK");
-												ok(page2.embedded, "embedded OK");
-												ok(page2.links.prev, "links.prev OK");
-												ok(page2.links.next, "links.next OK");
-												ok(page2.embedded.collection, "_embedded.items OK");
+												ok(page2.links, "Page 2: links OK");
+												ok(page2.embedded, "Page 2: embedded OK");
+												ok(page2.links.prev, "Page 2: links.prev OK");
+												ok(page2.links.next, "Page 2: links.next OK");
+												ok(page2.embedded.collection, "Page 2: _embedded.items OK");
 											}
 										)
 								}
